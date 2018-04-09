e465bfd9c9e591a761d4114afdbb78a648e039a0
Aline Favretto <anfavretto@hotmail.com>
2018-04-09 00:26:22 -0300

    Adicionar consistência nas ligações (não permitir transicao -> transicao, nem lugar -> lugar)

--- rede-petri.js
+++ rede-petri.js
@@ -1,54 +1,54 @@
-class Lugar 
-{
-	constructor(nome, marcas)
-	{
+class Lugar {
+	constructor(nome, marcas) {
 		this.nome = nome;
 		this.marcas = marcas;
 	}
 }
 
-class Transicao
-{
-	constructor(nome)
-	{
+class Transicao {
+	constructor(nome) {
 		this.nome = nome;
 		this.habilitado = false;
 	}
 }
 
-class RedePetri
-{
-	constructor()
-	{
-		this.vertice    = [];
-		this.nVertices  = 0;
-		this.arco       = [];
-		this.ciclo      = 0;
-		this.habilitados= [];
-		this.passo      = 0;
+class RedePetri {
+	constructor() {
+		this.vertice = [];
+		this.nVertices = 0;
+		this.arco = [];
+		this.ciclo = 0;
+		this.habilitados = [];
+		this.passo = 0;
 	}
 
-	adicionaVertice(vertice)
-	{
-		this.vertice[ this.nVertices++ ] = vertice;
+	adicionaVertice(vertice) {
+		this.vertice[this.nVertices++] = vertice;
 	}
 
-	adicionaArco(v1, v2, peso = 1)
-	{
-		// TODO: adicionar consistência nas ligações (não permitir transicao -> transicao, nem lugar -> lugar)
+	adicionaArco(v1, v2, peso = 1) {		
+		this.validarVertices(v1, v2);
+
 		if (!this.arco[v1])
-			this.arco[v1]     = [];
+			this.arco[v1] = [];
 
 		this.arco[v1][v2] = peso;
 
 	}
 
-	localizaVertice(nome)
-	{
-		for(var i = 0; i < this.vertice.length; i++)
-		{
-			if (this.vertice[i].nome == nome)
-			{
+	validarVertices(v1, v2) {
+		if (this.vertice[v1] instanceof Lugar && this.vertice[v2] instanceof Lugar) {
+			throw "Não é possível adicionar arcos entre Lugares (" + this.vertice[v1].nome + ", " + this.vertice[v2].nome + ").";
+		}
+
+		if (this.vertice[v1] instanceof Transicao && this.vertice[v2] instanceof Transicao) {
+			throw "Não é possível adicionar arcos entre Transições (" + this.vertice[v1].nome + ", " + this.vertice[v2].nome + ").";
+		}
+	}
+
+	localizaVertice(nome) {
+		for (var i = 0; i < this.vertice.length; i++) {
+			if (this.vertice[i].nome == nome) {
 				return i;
 			}
 		}
@@ -57,31 +57,23 @@ class RedePetri
 	}
 
 	/* Percorre todos o nós, executando 1 passo*/
-	executa()
-	{
+	executa() {
 		// Percorre as transações habilitadas, atualizando os pesos
-		for(var i = 0; i < this.habilitados.length; i++)
-		{
+		for (var i = 0; i < this.habilitados.length; i++) {
 			// Percorre os arcos deste vértice
-			for(var j = 0; j < this.arco.length; j++)
-			{
+			for (var j = 0; j < this.arco.length; j++) {
 				// Para
-				if (j == this.habilitados[i])
-				{
-					for (var k = 0; k < this.arco[j].length; k++)
-					{
-						if (this.arco[j][k] != undefined)
-						{
+				if (j == this.habilitados[i]) {
+					for (var k = 0; k < this.arco[j].length; k++) {
+						if (this.arco[j][k] != undefined) {
 							this.vertice[k].marcas += this.arco[j][k];
 						}
 					}
 				}
 
 				// De
-				if (this.arco[j])
-				{
-					if (this.arco[j][this.habilitados[i]] != undefined)
-					{
+				if (this.arco[j]) {
+					if (this.arco[j][this.habilitados[i]] != undefined) {
 						this.vertice[j].marcas -= this.arco[j][this.habilitados[i]];
 					}
 				}
@@ -94,15 +86,12 @@ class RedePetri
 
 	}
 
-	atualizaHabilitado()
-	{
+	atualizaHabilitado() {
 		this.habilitados = [];
 
 		// Percorre capturando as transacoes
-		for(var i = 0; i < this.nVertices; i++)
-		{
-			if (this.vertice[i] instanceof Transicao)
-			{
+		for (var i = 0; i < this.nVertices; i++) {
+			if (this.vertice[i] instanceof Transicao) {
 				this.vertice[i].habilitado = this.verificaTransacao(i);
 				if (this.vertice[i].habilitado)
 					this.habilitados.push(i);
@@ -111,22 +100,18 @@ class RedePetri
 
 	}
 
-	verificaTransacao(indice)
-	{
-		let transacao  = this.vertice[indice];
-		let local      = {};
+	verificaTransacao(indice) {
+		let transacao = this.vertice[indice];
+		let local = {};
 		let habilitado = false;
 
 		// Percorre os arcos verificando os pesos
-		for(var i = 0; i < this.arco.length; i++)
-		{
-			if (this.arco[i])
-			{
-				if (this.arco[i][indice] != undefined)
-				{
+		for (var i = 0; i < this.arco.length; i++) {
+			if (this.arco[i]) {
+				if (this.arco[i][indice] != undefined) {
 					local = this.vertice[i];
-						if (local.marcas >= this.arco[i][indice])
-							habilitado = true;
+					if (local.marcas >= this.arco[i][indice])
+						habilitado = true;
 					else
 						return false;
 				}
@@ -135,60 +120,54 @@ class RedePetri
 		return habilitado;
 	}
 
-	desenhaTerminal(ciclo)
-	{
+	desenhaTerminal(ciclo) {
 		let transicoes = [];
-		    transicoes['cabecalho'] = '';
-		    transicoes['dados'] = '';
-		let lugares    = [];
-			lugares['cabecalho'] = '';
-			lugares['dados'] = '';
-		let cabecalho  = '|Ciclo|  ';
-		let dados      = '|  ';
+		transicoes['cabecalho'] = '';
+		transicoes['dados'] = '';
+		let lugares = [];
+		lugares['cabecalho'] = '';
+		lugares['dados'] = '';
+		let cabecalho = '|Ciclo|  ';
+		let dados = '|  ';
 
-		dados         += this.ciclo + '  |  ';
+		dados += this.ciclo + '  |  ';
 
-		for(var i = 0; i < this.vertice.length; i++)
-		{
-			if (this.vertice[i] instanceof Lugar) 
-			{
+		for (var i = 0; i < this.vertice.length; i++) {
+			if (this.vertice[i] instanceof Lugar) {
 				//if (ciclo == 0)
-					lugares['cabecalho'] += this.vertice[i].nome + ' |  ';
+				lugares['cabecalho'] += this.vertice[i].nome + ' |  ';
 
 				lugares['dados'] += this.vertice[i].marcas + '  |  ';
 			} else {
 
 				//if (ciclo == 0)
-					transicoes['cabecalho'] += this.vertice[i].nome + ' |  ';
+				transicoes['cabecalho'] += this.vertice[i].nome + ' |  ';
 
 				transicoes['dados'] += ((this.vertice[i].habilitado) ? 'S' : 'N') + '  |  ';
 			}
 		}
 
 		cabecalho += lugares['cabecalho'] + transicoes['cabecalho'];
-		dados     += lugares['dados'] + transicoes['dados'];
+		dados += lugares['dados'] + transicoes['dados'];
 		console.log(cabecalho);
 		console.log(dados);
 		document.getElementById("redeOutput").innerHTML = cabecalho + "\n" + dados;
 	}
 
-	desenhaTabela(ciclo)
-	{
+	desenhaTabela(ciclo) {
 		let transicoes = [];
-		    transicoes['cabecalho'] = '';
-		    transicoes['dados'] = '';
-		let lugares    = [];
-			lugares['cabecalho'] = '';
-			lugares['dados'] = '';
-		let cabecalho  = '<th scope="col">Ciclo</th>';
-		let dados      = '<td>';
+		transicoes['cabecalho'] = '';
+		transicoes['dados'] = '';
+		let lugares = [];
+		lugares['cabecalho'] = '';
+		lugares['dados'] = '';
+		let cabecalho = '<th scope="col">Ciclo</th>';
+		let dados = '<td>';
 
-		dados         += this.ciclo + '</td>';
+		dados += this.ciclo + '</td>';
 
-		for(var i = 0; i < this.vertice.length; i++)
-		{
-			if (this.vertice[i] instanceof Lugar) 
-			{
+		for (var i = 0; i < this.vertice.length; i++) {
+			if (this.vertice[i] instanceof Lugar) {
 				if (ciclo == 0)
 					lugares['cabecalho'] += '<th scope="col">' + this.vertice[i].nome + '</th>';
 
@@ -203,7 +182,7 @@ class RedePetri
 		}
 
 		cabecalho += lugares['cabecalho'] + transicoes['cabecalho'];
-		dados     += lugares['dados'] + transicoes['dados'];
+		dados += lugares['dados'] + transicoes['dados'];
 		if (ciclo == 0)
 			$('.table thead').html('<tr>' + cabecalho + '</tr>');
 		$('.table tbody').html($('.table tbody').html() + '<tr>' + dados + '</tr>');
@@ -212,28 +191,35 @@ class RedePetri
 
 var rede = new RedePetri();
 
-function lerEntradaRede() 
-{
-	texto = document.getElementById("redeInput").value
-	let json = JSON.parse(texto);
+function lerEntradaRede() {
+	try {
+		texto = document.getElementById("redeInput").value
+		let json = JSON.parse(texto);
 
-	let lugares = json.L;
-	for(var i = 0; i < lugares.length; i++){
-		rede.adicionaVertice(new Lugar(lugares[i][0], lugares[i][1]));
-	}
-	let transicoes = json.T;
-	for(var i = 0; i < transicoes.length; i++){
-		rede.adicionaVertice(new Transicao(transicoes[i][0]));
-	}
-	let arcos = json.A;	
-	for(var i = 0; i < arcos.length; i++){
-		rede.adicionaArco(rede.localizaVertice(arcos[i][0]), rede.localizaVertice(arcos[i][1]), arcos[i][2]);
-	}
+		let lugares = json.L;
+		for (var i = 0; i < lugares.length; i++) {
+			rede.adicionaVertice(new Lugar(lugares[i][0], lugares[i][1]));
+		}
+		let transicoes = json.T;
+		for (var i = 0; i < transicoes.length; i++) {
+			rede.adicionaVertice(new Transicao(transicoes[i][0]));
+		}
+		let arcos = json.A;
+		for (var i = 0; i < arcos.length; i++) {
+			rede.adicionaArco(rede.localizaVertice(arcos[i][0]), rede.localizaVertice(arcos[i][1]), arcos[i][2]);
+		}
 
-	rede.atualizaHabilitado();
+		rede.atualizaHabilitado();
 
-	//rede.desenhaTerminal(0);
-	rede.desenhaTabela(0);
+		//rede.desenhaTerminal(0);
+		rede.desenhaTabela(0);
+	}
+	catch (e)
+	{
+		console.log("Erro " + e);
+		document.getElementById("erros").textContent = e;
+		//document.getElementById("erros").innerHTML = e.message;
+	}
 }
 
 /*
